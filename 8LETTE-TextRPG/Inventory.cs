8faa1f192499aeffa6e072c972ed8700abc85151
Hyeon O <115928656+gusdh8380@users.noreply.github.com>
2025-04-22 11:31:17 +0900

    feat : 인벤토리 클래스 추가
    장착하는 메소드와 해제하는 메소드 구현

---
+++ 8LETTE-TextRPG/Inventory.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8LETTE_TextRPG
+{
+    //인벤토리 클래스
+    // 가장 기본적인 기능만 구현하였습니다.
+
+    //장착 메소드 구현, 장착 시 능력치 갱신 구현
+    //+ 이미 장착 중인 경우 기존 아이템과 교체 로직 구현
+    //아이템 해제 메소드 구현
+    //UI 콘솔 출력은 제외했습니다.(스크린 담당)
+
+
+
+    public class Inventory
+    {
+        public readonly Player _player;
+        public readonly List<Item> _items = new List<Item> ();
+
+        public Inventory(Player player) => _player = player;
+        public void AddItem(Item item) => _items.Add(item);
+        public List<Item> GetAllItems() => _items;
+        public void DeleteItem(Item item) => _items.Remove(item);
+
+        public float EquippedAttackBonus() => _items.Where(i => i.IsEquipped).Sum(i => i.Attack);
+        public float EquippedDefenseBonus() => _items.Where(i => i.IsEquipped).Sum(i => i.Defense);
+
+
+        //장착 메소드
+        //입력된 수를 파라미터로 받아서 처리하도록 구현함
+        //잘못된 입력에 대해서는 구현하지 않았습니다.
+        public void Equip(int index)
+        {
+            var selected = _items[index];
+
+            if (selected.IsEquipped)
+            {
+                selected.IsEquipped = false;
+            }
+            else
+            {
+                // 같은 타입 해제
+                foreach (var item in _items)
+                {
+                    if (item.Type == selected.Type && item.IsEquipped)
+                        item.IsEquipped = false;
+                }
+                selected.IsEquipped = true;
+            }         
+        }
+        // 장착 해제
+        
+        public void Unequip(int index)
+        {
+            var item = _items[index];
+
+            if (item.IsEquipped)
+            {
+                item.IsEquipped = false;             
+            }
+        }
+
+    }
+}

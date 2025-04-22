147ab11a0b1ef485e368c850ad29c1ccfc07be60
Hyeon O <115928656+gusdh8380@users.noreply.github.com>
2025-04-22 09:49:02 +0900

    feat : Item 클래스

---
+++ 8LETTE-TextRPG/Item..cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8LETTE_TextRPG
+{
+    public class Item
+    {
+        public string id {  get; set; }
+        public string Name { get; }
+        public float Attack { get; }
+        public float Defense { get; }
+        public string Description { get; }
+        public float Price { get; }
+        public int Type { get; }
+        
+
+        public Item(string name, float atk, float def, string desc, float price, int type)
+        {
+            Name = name; Attack = atk; Defense = def; Description = desc; Price = price; Type = type;
+        }
+
+        //public string StatString()
+        //{
+        //    if (Attack > 0) return $"공격력 +{Attack}";
+        //    if (Defense > 0) return $"방어력 +{Defense}";
+        //    return string.Empty;
+        //}
+    }
+}

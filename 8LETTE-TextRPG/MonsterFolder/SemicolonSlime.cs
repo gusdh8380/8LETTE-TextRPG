62ddbf513b23197c8e1d8210fef2e7b8616124b6
김준희 <38108521+madkit1231@users.noreply.github.com>
2025-04-23 19:10:18 +0900

    feat : 몬스터 추가
    Lv 1 : SemicolonSlilme 추가
    Lv 2 : TypeMissGoblin 추가
    Lv 4 : IndexFairy 추가
    Lv 5 : Null 추가

---
+++ 8LETTE-TextRPG/MonsterFolder/SemicolonSlime.cs
@@ -0,0 +1,59 @@
+﻿namespace _8LETTE_TextRPG.MonsterFolder
+{
+
+    class SemicolonSlime : Monster
+    {
+        public SemicolonSlime()
+        {
+            Name = "세미콜론";
+            Level = 1;
+            MaxHp = 15f;
+            Hp = MaxHp;
+            Attack = 3f;
+            Defense = 2f;
+        }
+
+        protected override void DefineStates()
+        {
+            AddState(State.Idle, new StateElem
+            {
+                Doing = new Action(OnIdle)
+            });
+
+            AddState(State.Attack, new StateElem
+            {
+                Doing = new Action(AttackDoing)
+            });
+
+            AddState(State.Dead, new StateElem
+            {
+                Doing = new Action(OnDeath)
+            });
+        }
+
+        private void OnIdle()
+        {
+
+        }
+
+        private void AttackDoing()
+        {
+            AttackTo(Player.Instance);
+        }
+
+        private void OnDeath()
+        {
+
+        }
+
+        public override void OnDamaged(float dmg)
+        {
+            base.OnDamaged(dmg);
+        }
+
+        public override void AttackTo(Player victim)
+        {
+            base.AttackTo(victim);
+        }
+    }
+}
